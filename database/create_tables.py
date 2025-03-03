f1128ea7f84285b3fcd003fec53832b48f730f99
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2025-03-03 11:48:22 +0800

    Added new table

--- database/create_tables.py
+++ database/create_tables.py
@@ -19,6 +19,17 @@
 
 from db import cursor, connection
 
+'''
+Table in total:
+1. page_relationships
+2. url_mapping
+3. word_mapping
+4. forward_index
+5. inverted_index
+6. keyword_statistics
+7. page_rank
+'''
+
 sql_statements = [
     """
         CREATE TABLE IF NOT EXISTS page_relationships (
@@ -34,11 +45,45 @@ sql_statements = [
         );
     """,
     """
+        CREATE TABLE IF NOT EXISTS word_mapping (
+            word_id INT PRIMARY KEY,
+            word TEXT UNIQUE NOT NULL
+        )
+    """,
+    """
         CREATE TABLE IF NOT EXISTS forward_index (
             page_id INT PRIMARY KEY,
             title TEXT NOT NULL,
             last_modified_date TIMESTAMP NOT NULL,
             size INT NOT NULL,
+            child_links JSON NOT NULL,
+            FOREIGN KEY (page_id) REFERENCES url_mapping (page_id)
+        );
+    """,
+    """
+        CREATE TABLE IF NOT EXISTS inverted_index (
+            word_id INT NOT NULL,
+            page_id INT NOT NULL,
+            term_frequency INT NOT NULL,
+            PRIMARY KEY (word_id, page_id)
+            FOREIGN KEY (word_id) REFERENCES word_mapping (word_id),
+            FOREIGN KEY (page_id) REFERENCES url_mapping (page_id),
+        );
+    """,
+    """
+        CREATE TABLE IF NOT EXISTS keyword_statistics (
+            word_id INT NOT NULL,
+            page_id INT NOT NULL,
+            tf_idf FLOAT NOT NULL,
+            PRIMARY KEY (word_id, page_id),
+            FOREIGN KEY (word_id) REFERENCES word_mapping (word_id)
+            FOREIGN KEY (page_id) REFERENCES url_mapping (page_id)
+        );
+    """,
+    """
+        CREATE TABLE IF NOT EXISTS page_rank (
+            page_id INT PRIMARY KEY,
+            page_rank FLOAT NOT NULL,
             FOREIGN KEY (page_id) REFERENCES url_mapping (page_id)
         );
     """
