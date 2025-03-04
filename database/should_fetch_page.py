cd1d3b2624c3743fb71bd88edc4895a8bb8a26dd
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2025-03-04 13:36:09 +0800

    Updated

--- database/should_fetch_page.py
+++ database/should_fetch_page.py
@@ -21,16 +21,27 @@ def should_fetch_page(url: str, last_modified: str):
         bool: True if the page should be fetched, False otherwise.
     """
 
-    sql = "SELECT url, last_modified FROM forward_index WHERE url = %s"
+    #Get paeg_id from url_mapping table
+    sql = "SELECT page_id FROM url_mapping WHERE url = ?"
     cursor.execute(sql, (url,))
+    page_id = cursor.fetchone()
+
+    if page_id is None:
+        return True
+
+
+    sql = "SELECT last_modified_date FROM forward_index WHERE page_id = ?"
+    cursor.execute(sql, (page_id[0],))
     result = cursor.fetchone()
 
-    if result is None:
-        return True
-    elif result[1] == last_modified:
+    if result[0] == last_modified:
         return False
     else:
         return True
     
-    
+
     #raise NotImplementedError()
+
+#Test
+print(should_fetch_page("https://www.example.com", "Tue, 16 May 2023 05:03:16 GMT")) #True
+print(should_fetch_page("https://www.example.com/page1", "Tue, 16 May 2023 05:03:16 GMT")) #False
