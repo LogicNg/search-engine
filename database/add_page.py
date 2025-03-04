01990ec06c24569593e139653264105490c40bf8
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2025-03-04 12:19:14 +0800

    Updated

--- database/add_page.py
+++ database/add_page.py
@@ -1,3 +1,10 @@
+
+from db import cursor, connection
+import hashlib
+import json
+
+page_ids = []
+
 def add_page(
     body_text: str,
     last_modified: str,
@@ -28,6 +35,52 @@ def add_page(
         Check the database schema to know what fields to insert.
     """
 
-    
+    # Hash the URL to generate a unique identifier for the page
+    page_id = hashlib.md5(url.encode()).hexdigest()
+    page_id = int(page_id[:8], 16)
+    print(page_id)
 
-    #raise NotImplementedError()
+    if page_id in page_ids:
+        return
+    
+    page_ids.append(page_id)
+
+    try:
+        #Insert the page into the url_mapping table
+        sql = "INSERT INTO url_mapping (page_id, url) VALUES (?, ?)"
+        cursor.execute(sql, (page_id, url))
+
+        #Get the parent page id from table url_mapping
+        sql = "SELECT page_id FROM url_mapping WHERE url = ?"
+        cursor.execute(sql, (parent_url,))
+        parent_page_id = cursor.fetchone()
+
+        if parent_page_id is not None:
+            parent_page_id = parent_page_id[0]
+        else:
+            parent_page_id = None
+
+
+        #Insert the page into the forward_index table
+        sql = "INSERT INTO forward_index (page_id, title, last_modified_date, size, child_links) VALUES (?, ?, ?, ?, ?)"
+        cursor.execute(sql, (page_id, title, last_modified, len(body_text), json.dumps([])))
+
+        #Insert the page into the page_relationships table
+        sql = "INSERT INTO page_relationships (parent_page_id, child_page_id) VALUES (?, ?)"
+        cursor.execute(sql, (parent_page_id, page_id))
+
+        connection.commit()
+    
+    except Exception as e:
+        print(e)
+        connection.rollback()
+        raise e
+
+
+add_page(
+    body_text="This is the body text of the page.",
+    last_modified="Tue, 16 May 2023 05:03:16 GMT",
+    parent_url="https://www.example.com/page1",
+    title="Example Page",
+    url="https://www.example.com/page1",
+)
