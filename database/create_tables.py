10198b508815967579f72caeb481c9a4ce3604d7
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2025-03-04 13:36:14 +0800

    Updated

--- database/create_tables.py
+++ database/create_tables.py
@@ -1,22 +1,3 @@
-# CREATE TABLE page_relationships (
-#     parent_page_id INT NOT NULL,
-#     child_page_id INT NOT NULL,
-#     PRIMARY KEY (parent_page_id, child_page_id)
-# );
-
-# CREATE TABLE url_mapping (
-#     page_id INT PRIMARY KEY,
-#     url TEXT UNIQUE NOT NULL
-# );
-
-# CREATE TABLE forward_index (
-#     page_id INT PRIMARY KEY,
-#     title TEXT NOT NULL,
-#     last_modified_date TIMESTAMP NOT NULL,
-#     size INT NOT NULL,
-#     FOREIGN KEY (page_id) REFERENCES url_mapping (page_id)
-# );
-
 from db import cursor, connection
 
 '''
