cf9ccde7e813f7ba8b79b6685f8e9ff68a11d85d
Ng Lok Chun <nglokchun@gmail.com>
2025-03-19 11:38:50 +0800

    Refactor fetch_data to use CTEs

--- make_spider_result_txt.py
+++ make_spider_result_txt.py
@@ -2,21 +2,39 @@ from database.db import connection, cursor
 
 
 def fetch_data():
-    # Query to fetch all necessary data
+    # Query to fetch all necessary data using CTEs
     query = """
+    WITH keyword_data AS (
+        SELECT 
+            url, 
+            GROUP_CONCAT(word || ' ' || term_frequency, '; ' ORDER BY term_frequency DESC) AS keywords
+        FROM 
+            inverted_index
+        GROUP BY 
+            url
+    ),
+    child_link_data AS (
+        SELECT 
+            parent_url, 
+            GROUP_CONCAT(child_url, '; ' ORDER BY child_url) AS child_links
+        FROM 
+            page_relationships
+        GROUP BY 
+            parent_url
+    )
     SELECT 
         fi.url, 
         fi.title, 
         fi.last_modified_date, 
         fi.size, 
-        GROUP_CONCAT(ii.word || ' ' || ii.term_frequency, '; ') AS keywords,
-        GROUP_CONCAT(pr.child_url, '; ') AS child_links
+        kd.keywords,
+        cld.child_links
     FROM 
         forward_index fi
     LEFT JOIN 
-        inverted_index ii ON fi.url = ii.url
+        keyword_data kd ON fi.url = kd.url
     LEFT JOIN 
-        page_relationships pr ON fi.url = pr.parent_url
+        child_link_data cld ON fi.url = cld.parent_url
     GROUP BY 
         fi.url
     """
@@ -28,6 +46,7 @@ def format_data(data):
     formatted_data = []
     for row in data:
         url, title, last_modified_date, size, keywords, child_links = row
+
         # Format the keywords and child links
         keywords = keywords.split("; ")[:10] if keywords else []
         child_links = child_links.split("; ")[:10] if child_links else []
@@ -35,8 +54,9 @@ def format_data(data):
         # Format the entry
         entry = f"{title}\n{url}\n{last_modified_date}, {size}\n"
         entry += "; ".join(keywords) + "\n"
-        entry += "\n".join(child_links) + "\n"
-        entry += "-" * 30  # Separator line
+        if child_links:
+            entry += "\n".join(child_links) + "\n"
+        entry += "\n" + "-" * 30 + "\n"  # Separator line
         formatted_data.append(entry)
     return formatted_data
 
