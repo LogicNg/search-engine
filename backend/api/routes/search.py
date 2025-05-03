69d0585a2470cc910b4759cc3e74d2b61abb8d1f
Ng Lok Chun <nglokchun@gmail.com>
2025-05-03 23:38:06 +0800

    Extract levenshtein_distance() to file

--- backend/api/routes/search.py
+++ backend/api/routes/search.py
@@ -2,7 +2,8 @@ import math
 
 from flask import Blueprint, jsonify, request
 
-from api.get_cursor import get_cursor
+from api.utils.get_cursor import get_cursor
+from api.utils.levenshtein_distance import levenshtein_distance
 
 search_bp = Blueprint("search", __name__)
 
@@ -102,36 +103,6 @@ pages = [
     },
 ]
 """
-
-
-def levenshtein_distance(s1, s2):
-    """
-    Calculate the Levenshtein distance (edit distance) between two strings.
-
-    Args:
-        s1: First string
-        s2: Second string
-
-    Returns:
-        int: The minimum number of single-character edits needed to change one string into the other
-    """
-    if len(s1) < len(s2):
-        return levenshtein_distance(s2, s1)
-
-    if len(s2) == 0:
-        return len(s1)
-
-    previous_row = range(len(s2) + 1)
-    for i, c1 in enumerate(s1):
-        current_row = [i + 1]
-        for j, c2 in enumerate(s2):
-            insertions = previous_row[j + 1] + 1
-            deletions = current_row[j] + 1
-            substitutions = previous_row[j] + (c1 != c2)
-            current_row.append(min(insertions, deletions, substitutions))
-        previous_row = current_row
-
-    return previous_row[-1]
 
 
 def calculate_cosine_similarity(
