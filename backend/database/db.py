d013788cfbf23b4075c559924fee55f35d19f284
Ng Lok Chun <nglokchun@gmail.com>
2025-05-03 22:11:21 +0800

    Add calculate_page_rank to main.py

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,9 +1,6 @@
 import os
 import sqlite3
 
-# if os.path.exists("documents.db"):
-#     os.remove("documents.db")
-
 # Global variables for easy access
 connection = sqlite3.connect("documents.db")
 cursor = connection.cursor()
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
+import os
+
+if os.path.exists("documents.db"):
+    os.remove("documents.db")
+
 from api.app import app
 from crawler.fetch_and_save_pages import fetch_and_save_pages
 from database.create_tables import create_tables
-from database.page_rank import plot_page_graph
+from database.page_rank import calculate_page_rank, plot_page_graph
 from make_spider_result_txt import make_spider_result_txt
 
 create_tables()
 fetch_and_save_pages("https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm")
-#make_spider_result_txt()
-app.run(debug=True)
+calculate_page_rank()
+
+# make_spider_result_txt()
+# app.run(debug=True)
