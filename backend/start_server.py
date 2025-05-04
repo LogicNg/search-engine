8f3ca3af771ffb340f679be7d84b3d8899e644d4
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2025-05-04 19:40:12 +0800

    Added app_context check

--- backend/start_server.py
+++ backend/start_server.py
@@ -1,8 +1,11 @@
 from api.app import app
+#from api.routes.search_logic import search_bp
 from api.routes.search import search_bp
 from api.routes.suggestions import suggestions_bp
 
 app.register_blueprint(search_bp)
 app.register_blueprint(suggestions_bp)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
