918c985dcd09d9c8e586c96d9cd5f32ed079dbb2
PakNinnnn <107379369+PakNinnnn@users.noreply.github.com>
2025-05-10 20:35:59 +0800

    Removed plotting page rank graph

--- backend/crawl_and_save.py
+++ backend/crawl_and_save.py
@@ -14,7 +14,7 @@ if os.path.exists("documents.db"):
 
 from crawler.fetch_and_save_pages import fetch_and_save_pages
 from database.create_tables import create_tables
-from database.page_rank import calculate_page_rank, plot_page_graph
+from database.page_rank import calculate_page_rank
 
 create_tables()
 fetch_and_save_pages("https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm")
